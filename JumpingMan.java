e870b8305f7fb342dd6fb7b68d777dffe03fe0d2
Timbo0456 <84857278+Timbo0456@users.noreply.github.com>
2021-05-31 15:11:49 -0400

    Add files via upload

---
+++ JumpingMan.java
@@ -0,0 +1,118 @@
+
+public class JumpingMan {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		 do {
+		    	
+			    System.out.println("                        ");		  
+			    System.out.println("     _______            ");				
+			    System.out.println("     | _  _|            ");
+			    System.out.println("     | ____|            ");				    
+			    System.out.println("     |_____|            ");
+			    System.out.println("        |               ");
+			    System.out.println("        |               ");
+			    System.out.println("        |               ");
+			    System.out.println("    ----|----           ");
+			    System.out.println("        |               ");
+			    System.out.println("       / \\             ");
+			    System.out.println("      /   \\            ");
+			    System.out.println("     /     \\           ");
+			    System.out.println("    /       \\          ");
+			    
+			    Thread.sleep(200);
+			
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    
+			    System.out.println("     _______            ");				
+			    System.out.println("     | ^  ^|            ");
+			    System.out.println("     | ____|            ");				    
+			    System.out.println("     |_____|            ");
+			    System.out.println("        |               ");
+			    System.out.println("     \\  |  /           ");
+			    System.out.println("      \\ | /            ");
+			    System.out.println("       \\|/             ");
+			    System.out.println("        |               ");
+			    System.out.println("       / \\             ");
+			    System.out.println("       / \\             ");
+			    System.out.println("      /   \\            ");
+			    System.out.println("     /     \\           ");
+			    System.out.println("    /       \\          ");
+			    Thread.sleep(200);
+			    
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    
+			    System.out.println("     _______            ");				
+			    System.out.println("     | ^  ^|            ");
+			    System.out.println("     | ____|            ");				    
+			    System.out.println("     |_____|            ");
+			    System.out.println("        |               ");
+			    System.out.println("     \\  |  /           ");
+			    System.out.println("      \\ | /            ");
+			    System.out.println("       \\|/             ");
+			    System.out.println("        |               ");
+			    System.out.println("       / \\             ");
+			    System.out.println("   \\  /   \\  /        ");
+			    System.out.println("    \\/     \\/         ");
+			    System.out.println("                        ");
+			    System.out.println("                        ");
+			    Thread.sleep(200);			    
+			    
+			    
+		/*	    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    
+			    Thread.sleep(100);
+		*/
+			//Cadre JUMPING MAN
+
+			    
+			    Thread.sleep(200);
+		/*	    
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    System.out.println("                       ");
+			    
+			    Thread.sleep(100);
+		*/    
+			    } while (true);
+		}
+
+	}
+
+
